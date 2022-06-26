017fada38dcd18e37f638510c7a02c6d982a7570
Holger Schmidt <leroyz.mailaccount@gmx.net>
2022-06-26 18:09:36 +0200

    added commands for upgrading and other useful tasks

--- publish.cmd
+++ publish.cmd
@@ -1,1 +1,7 @@
+#dotnet --list-runtimes
+#dotnet list package
+#dotnet list package --outdated
+#dotnet add package YoutubeExplode
+#dotnet add package CommandLineParser
+#dotnet add package AngleSharp
 dotnet publish --configuration release
