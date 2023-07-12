7f8bb7e40532b283cc29314a21a152e12d4d8110
Holger Schmidt <leroyz.mailaccount@gmx.net>
2023-07-12 12:56:40 +0200

    fixup! shortening dynamic field name for Captions

--- SubTubular/CacheModels.cs
+++ SubTubular/CacheModels.cs
@@ -86,7 +86,10 @@ namespace SubTubular
 
         /// <summary>For indexing <see cref="CaptionTrack"/>s with a <see cref="Video"/>
         /// as dynamic fields identifyable by <see cref="LanguageName"/>.</summary>
-        internal string FieldName => fieldName ??= LanguageName.UpperCaseFirstLetters().ReplaceNonWordCharacters() + FieldSuffix;
+        internal string FieldName => fieldName ??= LanguageName
+            .Replace(" (auto-generated)", "Auto") // to shorten field name
+            /* upper-case first letters to preserve word boundary and remove chars preventing use in field queries */
+            .UpperCaseFirstLetters().ReplaceNonWordCharacters() + FieldSuffix;
         #endregion
     }
 
