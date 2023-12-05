d5eab0acf84385d7b9c64fb726ca1bd18d47da43
Holger Schmidt <leroyz.mailaccount@gmx.net>
2023-12-05 02:21:42 +0100

    fixup! started added search page

--- Ui/App.fs
+++ Ui/App.fs
@@ -1,5 +1,6 @@
 namespace Ui
 
+open Avalonia.Controls
 open Avalonia.Themes.Fluent
 open Fabulous
 open Fabulous.Avalonia
@@ -21,6 +22,7 @@ module App =
     }
 
     type Msg =
+        | ScopeChanged of SelectionChangedEventArgs
         | AliasesUpdated of string
         | QueryChanged of string
         | Search of bool
@@ -50,6 +52,7 @@ module App =
 
     let update msg model =
         match msg with
+        | ScopeChanged args -> { model with Scope = args.AddedItems.Item 0 :?> Scopes }, Cmd.none
         | AliasesUpdated txt -> { model with Aliases = txt }, Cmd.none
         | QueryChanged txt -> { model with Query = txt }, Cmd.none
         | Search on -> { model with Searching = on }, (if on then searchCmd model else Cmd.none)
@@ -68,23 +71,29 @@ module App =
                 https://github.com/fabulous-dev/Fabulous.Avalonia/tree/main/src/Fabulous.Avalonia/Views
                 https://play.avaloniaui.net/ *)
     let view model =
-        (VStack() {
+        Grid(coldefs = [Star], rowdefs = [Auto; Auto]) {
 
-            (HStack() {
+            Grid(coldefs = [Auto; Star; Auto; Star; Auto], rowdefs = [Auto]) {
                 (ComboBox(System.Enum.GetValues<Scopes>(), fun scope -> ComboBoxItem(scope.ToString())))
-                    .selectedItem(model.Scope)
+                    .selectedItem(model.Scope).onSelectionChanged(ScopeChanged).gridColumn(0)
                 TextBox(model.Aliases, AliasesUpdated)
-                TextBlock("for").centerVertical()
+                    .watermark("by " + (if model.Scope = Scopes.videos then "space-separated IDs or URLs"
+                        elif model.Scope = Scopes.playlist then "ID or URL"
+                        else "handle, slug, user name, ID or URL"))
+                    .gridColumn(1)
+                TextBlock("for")
+                    .margin(10, 0).centerVertical().gridColumn(2)
                 TextBox(model.Query, QueryChanged)
+                    .watermark("your query")
+                    .gridColumn(3)
                 ToggleButton((if model.Searching then "Stop" else "Search"), model.Searching, Search)
-            })
-                .margin(20.)
-                .centerHorizontal()
+                    .margin(10, 0)
+                    .gridColumn(4)
+            }
 
             Button("Reset", Reset).centerHorizontal()
 
-        })
-            .center()
+        }
 
     
 #if MOBILE
