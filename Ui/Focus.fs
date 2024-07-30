15e6448c293b68ddd7735a2e41d1e72f5628d780
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-07-31 01:02:45 +0200

    fixed giving AutoComplete focus and resetting Added flag

--- Ui/Focus.fs
+++ Ui/Focus.fs
@@ -1,25 +1,32 @@
 ﻿namespace Ui
 
 open System.Runtime.CompilerServices
+open Avalonia.Controls
 open Avalonia.Input
 open Fabulous
 open Fabulous.Avalonia
 
 module FocusAttributes =
-    /// Allows setting the Focus on an Avalonia.Input.InputElement
+    /// Allows setting the Focus on an AutoCompleteBox
     let Focus =
-        Attributes.defineBool "Focus" (fun oldValueOpt newValueOpt node ->
-            let target = node.Target :?> InputElement
+        let rec focusOnce obj _ =
+            let autoComplete = unbox<AutoCompleteBox> obj
+            autoComplete.Focus(NavigationMethod.Unspecified) |> ignore
+            autoComplete.TemplateApplied.RemoveHandler(focusOnce) // to clean up
 
-            let rec onAttached obj args =
-                target.Focus() |> ignore
-                target.AttachedToVisualTree.RemoveHandler(onAttached) // to clean up
-
+        Attributes.defineBool "Focus" (fun _ newValueOpt node ->
             if newValueOpt.IsSome && newValueOpt.Value then
-                target.AttachedToVisualTree.AddHandler(onAttached))
+                let autoComplete = unbox<AutoCompleteBox> node.Target
+                autoComplete.TemplateApplied.RemoveHandler(focusOnce) // to avoid duplicate handlers
+
+                (*  Wait to call Focus() on AutoCompleteBox until after TemplateApplied
+                    because of internal Avalonia AutoCompleteBox implementation:
+                    FocusChanged only applies the Focus to the nested TextBox if it is set - which happens in OnApplyTemplate.
+                    See https://github.com/AvaloniaUI/Avalonia/blob/master/src/Avalonia.Controls/AutoCompleteBox/AutoCompleteBox.cs *)
+                autoComplete.TemplateApplied.AddHandler(focusOnce))
 
 type FocusModifiers =
+    /// Sets the Focus on an IFabAutoCompleteBox if set is true; otherwise does nothing.
     [<Extension>]
-    /// Sets the Focus on an IFabInputElement if set is true; otherwise does nothing.
-    static member inline focus(this: WidgetBuilder<'msg, #IFabInputElement>, set: bool) =
+    static member inline focus(this: WidgetBuilder<'msg, #IFabAutoCompleteBox>, set: bool) =
         this.AddScalar(FocusAttributes.Focus.WithValue(set))
