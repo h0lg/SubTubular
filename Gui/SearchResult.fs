aac7d0ce68840a21bc3cff57d287e91dfaa15ef9
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-08-23 13:37:36 +0200

    relayouted video search result to fix scrolling them while lazy-loading thumbnails

--- Gui/SearchResult.fs
+++ Gui/SearchResult.fs
@@ -39,23 +39,25 @@ module SearchResult =
     let render (matchPadding: uint32) (result: VideoSearchResult) =
         let videoUrl = Youtube.GetVideoUrl result.Video.Id
 
-        (VStack() {
-            Grid(coldefs = [ Auto; Auto; Star ], rowdefs = [ Auto; Auto ]) {
-                AsyncImage(result.Video.Thumbnail)
-                    .tappable(OpenUrl videoUrl |> Common, "start the video in the browser")
-                    .margin(0, 0, 5, 0)
-                    .gridRowSpan (2)
+        Grid(coldefs = [ Auto; Star ], rowdefs = [ Auto ]) {
+            AsyncImage(result.Video.Thumbnail)
+                .tappable(OpenUrl videoUrl |> Common, "start the video in the browser")
+                .top()
+                .height(90) // to avoid jitter when scrolling results while lazy-loading thumbnails
+                .margin (0, 0, 5, 0)
 
-                (match result.TitleMatches with
-                 | null -> SelectableTextBlock(result.Video.Title, CopyingToClipboard)
-                 | matches -> writeHighlightingMatches matches None)
-                    .fontSize(18)
-                    .gridColumn (1)
+            (VStack() {
+                Grid(coldefs = [ Star; Auto ], rowdefs = [ Auto ]) {
+                    (match result.TitleMatches with
+                     | null -> SelectableTextBlock(result.Video.Title, CopyingToClipboard)
+                     | matches -> writeHighlightingMatches matches None)
+                        .wrap()
+                        .fontSize (18)
 
-                TextBlock("📅" + result.Video.Uploaded.ToString())
-                    .tooltip("uploaded")
-                    .textAlignment(TextAlignment.Right)
-                    .gridColumn (2)
+                    TextBlock("📅" + result.Video.Uploaded.ToString())
+                        .tooltip("uploaded")
+                        .gridColumn (1)
+                }
 
                 let reference =
                     if result.Scope = null then
@@ -64,51 +66,52 @@ module SearchResult =
                         Scope.getIcon (result.Scope.GetType())
                         + result.Scope.SingleValidated.Playlist.Title
 
-                TextBlock(reference).smallDemoted().gridRow(1).gridColumn (1)
-            }
+                TextBlock(reference).smallDemoted ()
 
-            if result.DescriptionMatches <> null then
-                HStack() {
-                    (TextBlock "in description").demoted ()
+                if result.DescriptionMatches <> null then
+                    HStack() {
+                        (TextBlock "in description").demoted ()
 
-                    for matches in result.DescriptionMatches.SplitIntoPaddedGroups(matchPadding) do
-                        writeHighlightingMatches matches (Some matchPadding)
-                }
+                        for matches in result.DescriptionMatches.SplitIntoPaddedGroups(matchPadding) do
+                            writeHighlightingMatches matches (Some matchPadding)
+                    }
 
-            if result.KeywordMatches.HasAny() then
-                HStack() {
-                    (TextBlock "in keywords").demoted ()
+                if result.KeywordMatches.HasAny() then
+                    HStack() {
+                        (TextBlock "in keywords").demoted ()
 
-                    for matches in result.KeywordMatches do
-                        writeHighlightingMatches matches (Some matchPadding)
-                }
+                        for matches in result.KeywordMatches do
+                            writeHighlightingMatches matches (Some matchPadding)
+                    }
 
-            if result.MatchingCaptionTracks.HasAny() then
-                for trackResult in result.MatchingCaptionTracks do
-                    TextBlock(trackResult.Track.LanguageName).demoted ()
+                if result.MatchingCaptionTracks.HasAny() then
+                    for trackResult in result.MatchingCaptionTracks do
+                        TextBlock(trackResult.Track.LanguageName).demoted ()
 
-                    let displaysHour = trackResult.HasMatchesWithHours(matchPadding)
-                    let splitMatches = trackResult.Matches.SplitIntoPaddedGroups(matchPadding)
+                        let displaysHour = trackResult.HasMatchesWithHours(matchPadding)
+                        let splitMatches = trackResult.Matches.SplitIntoPaddedGroups(matchPadding)
 
-                    for matched in splitMatches do
-                        let (synced, captionAt) =
-                            trackResult.SyncWithCaptions(matched, matchPadding).ToTuple()
+                        for matched in splitMatches do
+                            let (synced, captionAt) =
+                                trackResult.SyncWithCaptions(matched, matchPadding).ToTuple()
 
-                        let offset =
-                            TimeSpan
-                                .FromSeconds(captionAt)
-                                .FormatWithOptionalHours()
-                                .PadLeft(if displaysHour then 7 else 5)
+                            let offset =
+                                TimeSpan
+                                    .FromSeconds(captionAt)
+                                    .FormatWithOptionalHours()
+                                    .PadLeft(if displaysHour then 7 else 5)
 
-                        Grid(coldefs = [ Auto; Star ], rowdefs = [ Auto ]) {
-                            (TextBlock offset)
-                                .tappable(
-                                    OpenUrl $"{videoUrl}?t={captionAt}" |> Common,
-                                    $"tap to start the video at this timestamp in the browser"
-                                )
-                                .margin(0, 0, 5, 0)
-                                .demoted ()
+                            Grid(coldefs = [ Auto; Star ], rowdefs = [ Auto ]) {
+                                (TextBlock offset)
+                                    .tappable(
+                                        OpenUrl $"{videoUrl}?t={captionAt}" |> Common,
+                                        $"tap to start the video at this timestamp in the browser"
+                                    )
+                                    .margin(0, 0, 5, 0)
+                                    .demoted ()
 
-                            (writeHighlightingMatches synced (Some matchPadding)).gridColumn (1)
-                        }
-        })
+                                (writeHighlightingMatches synced (Some matchPadding)).gridColumn (1)
+                            }
+            })
+                .gridColumn (1)
+        }
