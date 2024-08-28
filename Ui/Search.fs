2f3033aa9abeed288e137ec7c3d31bb932417e71
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-08-28 22:15:18 +0200

    added NumericUpDown to go directly to page

--- Ui/Search.fs
+++ Ui/Search.fs
@@ -527,6 +527,21 @@ module Search =
                                         .tapCursor()
                                         .tooltip ("previous page")
 
+                                    NumericUpDown(
+                                        1,
+                                        lastPage + 1us |> float,
+                                        page + 1us |> float |> Some,
+                                        fun p ->
+                                            GoToKeywordPage(
+                                                scope.Key,
+                                                match p with
+                                                | Some p -> uint16 p - 1us
+                                                | None -> page
+                                            )
+                                    )
+                                        .showButtonSpinner(false)
+                                        .tooltip ("spin using mouse wheel or enter page")
+
                                     Button("▶", GoToKeywordPage(scope.Key, page + 1us))
                                         .tapCursor()
                                         .tooltip ("next page")
