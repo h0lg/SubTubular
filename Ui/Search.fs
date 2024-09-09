2ea80aad330d5f1cc43cb050df1f5665153f555a
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-09-09 23:11:08 +0200

    doing heavy lifting on a background task

--- Ui/Search.fs
+++ Ui/Search.fs
@@ -153,7 +153,7 @@ module Search =
                 dispatch CommandCompleted
             }
             |> Async.AwaitTask
-            |> Async.StartImmediate
+            |> Async.Start
         |> Cmd.ofEffect
 
     let private saveOutput model =
