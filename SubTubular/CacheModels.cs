8c1433255b58ad2ad518d49d24e6dd7d18dce532
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-09-20 16:26:32 +0200

    moved Playlist into a separate file

--- SubTubular/CacheModels.cs
+++ SubTubular/CacheModels.cs
@@ -4,19 +4,6 @@ using SubTubular.Extensions;
 namespace SubTubular;
 
 [Serializable]
-public sealed class Playlist
-{
-    public required string Title { get; set; }
-    public required string ThumbnailUrl { get; set; }
-    public string? Channel { get; set; }
-    public DateTime Loaded { get; set; }
-
-    /// <summary>The <see cref="Video.Id"/>s and (optional) upload dates
-    /// of the videos included in the <see cref="Playlist" />.</summary>
-    public IDictionary<string, DateTime?> Videos { get; set; } = new Dictionary<string, DateTime?>();
-}
-
-[Serializable]
 public sealed class Video
 {
     internal const string StorageKeyPrefix = "video ";
---
+++ SubTubular/Playlist.cs
@@ -0,0 +1,14 @@
+﻿namespace SubTubular;
+
+[Serializable]
+public sealed class Playlist
+{
+    public required string Title { get; set; }
+    public required string ThumbnailUrl { get; set; }
+    public string? Channel { get; set; }
+    public DateTime Loaded { get; set; }
+
+    /// <summary>The <see cref="Video.Id"/>s and (optional) upload dates
+    /// of the videos included in the <see cref="Playlist" />.</summary>
+    public IDictionary<string, DateTime?> Videos { get; set; } = new Dictionary<string, DateTime?>();
+}
