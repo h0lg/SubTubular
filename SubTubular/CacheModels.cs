9bccd9c85940f9a18de1020bbf5af5e08e4908bb
Holger Schmidt <leroyz.mailaccount@gmx.net>
2024-10-16 23:32:40 +0200

    moved ChannelAliasMap into its own file

--- SubTubular/CacheModels.cs
+++ SubTubular/CacheModels.cs
@@ -86,23 +86,3 @@ public sealed class Caption
     // for comparing captions when finding them in a caption track
     public override int GetHashCode() => HashCode.Combine(At, Text);
 }
-
-/// <summary>Maps valid channel aliases by <see cref="Type"/> and <see cref="Value"/>
-/// to an accessible <see cref="ChannelId"/> or null if none was found.</summary>
-[Serializable]
-public sealed class ChannelAliasMap
-{
-    internal const string StorageKey = "known channel alias maps";
-
-    public required string Type { get; set; }
-    public required string Value { get; set; }
-    public string? ChannelId { get; set; }
-
-    internal static (string, string) GetTypeAndValue(object alias) => (alias.GetType().Name, alias.ToString()!);
-
-    internal static async Task<HashSet<ChannelAliasMap>> LoadList(DataStore dataStore)
-        => await dataStore.GetAsync<HashSet<ChannelAliasMap>>(StorageKey) ?? [];
-
-    internal static async Task SaveList(HashSet<ChannelAliasMap> maps, DataStore dataStore)
-        => await dataStore.SetAsync(StorageKey, maps);
-}
---
+++ SubTubular/ChannelAliasMap.cs
@@ -0,0 +1,21 @@
+﻿namespace SubTubular;
+
+/// <summary>Maps valid channel aliases by <see cref="Type"/> and <see cref="Value"/>
+/// to an accessible <see cref="ChannelId"/> or null if none was found.</summary>
+[Serializable]
+public sealed class ChannelAliasMap
+{
+    internal const string StorageKey = "known channel alias maps";
+
+    public required string Type { get; set; }
+    public required string Value { get; set; }
+    public string? ChannelId { get; set; }
+
+    internal static (string, string) GetTypeAndValue(object alias) => (alias.GetType().Name, alias.ToString()!);
+
+    internal static async Task<HashSet<ChannelAliasMap>> LoadList(DataStore dataStore)
+        => await dataStore.GetAsync<HashSet<ChannelAliasMap>>(StorageKey) ?? [];
+
+    internal static async Task SaveList(HashSet<ChannelAliasMap> maps, DataStore dataStore)
+        => await dataStore.SetAsync(StorageKey, maps);
+}
