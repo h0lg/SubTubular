ea59c27c396c95d26de0f54a0dfca519aae4f6de
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-05-14 21:29:44 +0200

    extracted Entity search into a separate file

--- SubTubular/Youtube.cs
+++ SubTubular/Youtube.cs
@@ -9,7 +9,7 @@ using Pipe = System.Threading.Channels.Channel; // to avoid conflict with Youtub
 
 namespace SubTubular;
 
-public sealed class Youtube(DataStore dataStore, VideoIndexRepository videoIndexRepo)
+public sealed partial class Youtube(DataStore dataStore, VideoIndexRepository videoIndexRepo)
 {
     public static string GetVideoUrl(string videoId) => "https://youtu.be/" + videoId;
     internal static string GetPlaylistUrl(string id) => "https://www.youtube.com/playlist?list=" + id;
@@ -620,48 +620,6 @@ public sealed class Youtube(DataStore dataStore, VideoIndexRepository videoIndex
     /// with the <paramref name="scope"/> supplied.</summary>
     private Func<string, CancellationToken, Task<Video>> CreateVideoLookup(CommandScope scope)
         => (videoId, cancellation) => GetVideoAsync(videoId, cancellation, scope);
-
-    public async Task<IEnumerable<YoutubeSearchResult>> SearchForChannelsAsync(string text, CancellationToken cancellation)
-        => await SearchedForCachedAsync(text, ChannelScope.StorageKeyPrefix, async (string text, CancellationToken cancellation) =>
-        {
-            var channels = await Client.Search.GetChannelsAsync(text, cancellation);
-            return channels.Select(c => new YoutubeSearchResult(c.Id, c.Title, c.Url, SelectUrl(c.Thumbnails)));
-        }, cancellation);
-
-    public async Task<IEnumerable<YoutubeSearchResult>> SearchForPlaylistsAsync(string text, CancellationToken cancellation)
-        => await SearchedForCachedAsync(text, PlaylistScope.StorageKeyPrefix, async (string text, CancellationToken cancellation) =>
-        {
-            var playlists = await Client.Search.GetPlaylistsAsync(text, cancellation);
-            return playlists.Select(pl => new YoutubeSearchResult(pl.Id, pl.Title, pl.Url, SelectUrl(pl.Thumbnails), pl.Author?.ChannelTitle));
-        }, cancellation);
-
-    public async Task<IEnumerable<YoutubeSearchResult>> SearchForVideosAsync(string text, CancellationToken cancellation)
-        => await SearchedForCachedAsync(text, Video.StorageKeyPrefix, async (string text, CancellationToken cancellation) =>
-        {
-            var videos = await Client.Search.GetVideosAsync(text, cancellation);
-            return videos.Select(v => new YoutubeSearchResult(v.Id, v.Title, v.Url, SelectUrl(v.Thumbnails), v.Author?.ChannelTitle));
-        }, cancellation);
-
-    /// <summary>Identifies scope search caches by being the second prefix after the StorageKeyPrefix identifying the scope type.</summary>
-    public const string SearchAffix = "search ";
-
-    private async Task<YoutubeSearchResult[]> SearchedForCachedAsync(string text, string keyPrefix,
-        Func<string, CancellationToken, Task<IEnumerable<YoutubeSearchResult>>> searchYoutubeAsync, CancellationToken cancellation)
-    {
-        if (cancellation.IsCancellationRequested) return [];
-        string key = keyPrefix + SearchAffix + text.ToFileSafe();
-        var cached = await dataStore.GetAsync<YoutubeSearchResult.Cache>(key);
-
-        if (cached == null || cached.Search != text || cached.Created.AddHours(1) < DateTime.Now)
-        {
-            if (cancellation.IsCancellationRequested) return [];
-            var mapped = await searchYoutubeAsync(text, cancellation);
-            cached = new YoutubeSearchResult.Cache(text, mapped.ToArray(), DateTime.Now);
-            await dataStore.SetAsync(key, cached);
-        }
-
-        return cached.Results;
-    }
 
     private static string SelectUrl(IReadOnlyList<Thumbnail> thumbnails) => thumbnails.MinBy(tn => tn.Resolution.Area)!.Url;
 }
