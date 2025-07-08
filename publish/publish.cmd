fb6862d87665623cae08b2814244cc705850f4f6
Holger Schmidt <leroyz.mailaccount@gmx.net>
2025-07-09 01:38:26 +0200

    converted publish script into an SDK style project to enable packing it

--- publish/publish.cmd
+++ publish/publish.cmd
@@ -4,13 +4,13 @@
 :: dotnet list package --outdated
 
 :: clean output (to prevent build fragment bleed when rebuilding different parts of the same version)
-dotnet msbuild publish.proj /t:CleanOutput /v:d
+dotnet msbuild publish.csproj /t:CleanOutput /v:d
 
 :: publish Shell
-dotnet msbuild publish.proj /t:PublishShell /v:d
+dotnet msbuild publish.csproj /t:PublishShell /v:d
 
 :: publish Gui
-dotnet msbuild publish.proj /t:PublishGui /v:d
+dotnet msbuild publish.csproj /t:PublishGui /v:d
 
 :: zip the output folder
-dotnet msbuild publish.proj /t:ZipOutput /v:d
+dotnet msbuild publish.csproj /t:ZipOutput /v:d
